6943327647019ec25a9b2b2924005b8eb76bb3a3
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-02 16:37:29 +0530

    Custom Module made date.js using node docs Complete.

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
-var items = ["Item 1", "Item 2", "Item 3"];
-var workItems = [];
+const items = ["Item 1", "Item 2", "Item 3"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/", function(req, res) {
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  var day = today.toLocaleDateString("en-US", options);
+  const day = date.getDay();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function(req, res) {
   console.log(req.body);
   let item = req.body.newItem;
-if(req.body.list==="Work"){
-  workItems.push(item);
-  res.redirect("/work");
-}else{
-  items.push(item);
-  res.redirect("/");
-}
- 
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 app.get("/work", function(req, res) {
@@ -40,7 +34,7 @@ app.get("/work", function(req, res) {
 app.post("/work", function(req, res) {
   let item = req.body.newItem;
   workItems.push(item);
- 
+
   res.redirect("/work");
   console.log(item);
 });
