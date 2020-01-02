fb25c64f9036a0d97a666461a968458134b46dc7
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-02 19:40:21 +0530

    MongoDB Mongoose connection with App Complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 const items = ["Item 1", "Item 2", "Item 3"];
@@ -10,9 +10,41 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true, useUnifiedTopology: true});
+
+const itemSchema={
+  name:String
+}
+
+const Item=mongoose.model("Item",itemSchema)
+
+//these are document created using the Item Model.
+const item1 = new Item({
+  name:"Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name:"This is the second item"
+});
+
+const item3 = new Item({
+  name:"This is the third item"
+});
+
+const defaultItems =[item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Successfully saved the default items to DB.");
+  }
+})
+
+
 app.get("/", function(req, res) {
-  const day = date.getDay();
-  res.render("list", { listTitle: day, newListItems: items });
+
+  res.render("list", { listTitle:"Today", newListItems: items });
 });
 
 app.post("/", function(req, res) {
