2140575e040a0455bf27f1ebd323b63042d9b232
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2020-01-04 22:59:23 +0530

    Rendering Database Items in the

--- app.js
+++ app.js
@@ -10,41 +10,47 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const itemSchema={
-  name:String
-}
+const itemSchema = {
+  name: String
+};
 
-const Item=mongoose.model("Item",itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 
 //these are document created using the Item Model.
 const item1 = new Item({
-  name:"Welcome to your todolist!"
+  name: "Welcome to your todolist!"
 });
 
 const item2 = new Item({
-  name:"This is the second item"
+  name: "This is the second item"
 });
 
 const item3 = new Item({
-  name:"This is the third item"
+  name: "This is the third item"
 });
 
-const defaultItems =[item1,item2,item3];
-
-Item.insertMany(defaultItems,function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Successfully saved the default items to DB.");
-  }
-})
-
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function(req, res) {
-
-  res.render("list", { listTitle:"Today", newListItems: items });
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved the default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
 });
 
 app.post("/", function(req, res) {
